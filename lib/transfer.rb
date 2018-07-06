dd58f1d38e8276c17af8e39d6c0271b25d6acf68
Will Lee <will.i.lee@outlook.com>
2018-07-06 19:05:05 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :recipient, :amount
+
+  def initialize(sender, recipient, amount)
+    @sender = sender
+    @recipient = recipient
+    @amount = amount
+  end
 end
