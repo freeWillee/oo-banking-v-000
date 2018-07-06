ffc69d836232c015e3894ed481362de3e30fb9b2
Will Lee <will.i.lee@outlook.com>
2018-07-06 19:13:40 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,9 +14,13 @@ class Transfer
   end
 
   def execute_transaction
-    self.sender.balance -= self.amount
-    self.recipient.balance += self.amount
-    self.status = "completed"
+    if self.valid?
+      self.sender.balance -= self.amount
+      self.recipient.balance += self.amount
+      self.status = "completed"
+    else
+      return "Transaction rejected. Please check your account balance."
+    
   end
 
 end
